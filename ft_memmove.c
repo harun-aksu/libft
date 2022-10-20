c17bab552adde60a3a4917ea907d24434ec1f13a
aksuharun <harun16x@gmail.com>
2022-10-21 00:19:56 +0300

    backing-up

--- ft_memmove.c
+++ ft_memmove.c
@@ -2,10 +2,29 @@
 
 void    *ft_memmove(void *dest, const void *src, size_t len)
 {
-   void *original_dst = dest;
-   if(dest>src)
-      //while()
+   void *original_dest = dest;
+
+   if(!dest && !src)
+      return(NULL);
+
+   if (dest>src)
+   {
+      len--;
+      while (n => 0)
+         *(char*)(dest+(len--)) = *(char*)(src+(len--));
+   }
+   else
+   {
+      size_t   i;   
+
+      i  =  0;
+      while (i < n)
+         *(char*)(dest+(i++)) = *(char*)(src+(i++));
+   }
    
+   return(original_dest)
+      
+       
             
 } 
 
--- libft.h
+++ libft.h
@@ -22,7 +22,7 @@
 		
 	void *ft_memcpy(void *dst, const void *src, size_t n);
 		
-	//void *ft_memmove(void *dest, const );
+	void *ft_memmove(void *dest, const void *src, size_t len);
 		
 	int ft_strlcpy(char *dst, const char *src, size_t dstsize);
 		
